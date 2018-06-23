8aaaf028e7fa184a4ade665fe89dd434dd9eb343
thescarletmanuka <martin.roberts@hapara.com>
2018-06-23 17:20:58 +1200

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,23 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
   ],
 }
